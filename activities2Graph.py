edd4f4991626230339c3a508adb9e015ec0a5fcc
kdaily <kdaily@users.noreply.github.com>
2017-03-01 08:34:47 -0800

    fix params

--- activities2Graph.py
+++ activities2Graph.py
@@ -1,11 +1,14 @@
 import synapseclient
 import load2Neo4jDB as ndb
-import convertSynapse2Graph as cg
+import convertSynapse2Graph
 import multiprocessing.dummy as mp
 import threading
 import argparse
 import logging
 import json
+import tempfile
+import sys
+
 from collections import OrderedDict
 from py2neo import Graph, authenticate
 
@@ -22,45 +25,48 @@ if __name__ == '__main__':
                             [2, default: graph.json] the name of json outfile to graph provenance and
                             [3, default: # of available cores] the mp pool size''')
     parser.add_argument('id', metavar='synId', nargs='+', help='Input the synapse ID or list of synapse IDs')
-    parser.add_argument('--j', metavar='json', help='Input name of json outfile')
-    parser.add_argument('--p', type=int, help='Specify the pool size for the multiprocessing module', default=2)
-    parser.add_argument('-l', action='store_true', default=False, help='Load data from json file to Neo4j database')
+    parser.add_argument('-n', type=int, help='Specify the pool size for the multiprocessing module', default=2)
+    parser.add_argument('-l', '--load', action='store_true', default=False, help='Load data from json file to Neo4j database')
     args = parser.parse_args()
 
-    proj_inputs = args.id
-    json_file = args.j
-    p = mp.Pool(args.p)
+    p = mp.Pool(args.n)
+
     nodes = dict()
 
-    with open(os.path.join(os.path.expanduser("~"), "credentials.json")) as creds:
-        db_info=json.load(creds)
+    if args.load:
+        with open(os.path.join(os.path.expanduser("~"), "credentials.json")) as creds:
+            db_info = json.load(creds)
+            logging.info("Loaded neo4j credentials.")
 
-    for proj in proj_inputs:
+    for proj in args.id:
         print 'Getting entities from %s' %proj
-        nodes.update(cg.getEntities(projectId = proj))
-    logging.info('Fetched %i entities' %len(nodes))
+        nodes.update(convertSynapse2Graph.processEntities(projectId = proj))
 
-    activities = p.map(cg.safeGetActivity, nodes.items())
-    activities = cg.cleanUpActivities(activities)
+    logging.info('Fetched %i entities' % len(nodes))
+
+    activities = p.map(convertSynapse2Graph.safeGetActivity, nodes.items())
+    activities = convertSynapse2Graph.cleanUpActivities(activities)
     if len(activities) > 0:
         print '%i activities found i.e. %f%% entities have provenance' %(len(activities),
                                                                             float(len(nodes))/len(activities))
     else:
         print 'This project lacks accessible information on provenance'
 
-    edges = cg.buildEdgesfromActivities(nodes, activities)
+    edges = convertSynapse2Graph.buildEdgesfromActivities(nodes, activities)
     logging.info('I have  %i nodes and %i edges' %(len(nodes), len(edges)))
-    with open(json_file, 'w') as fp:
-        json.dump(OrderedDict([('vertices', map(dict, nodes.values())), ('edges', edges)]), fp, indent=4)
 
-    if args.l:
-        logging.info('Connecting to Neo4j and authenticating user credentials')
-        authenticate(db_info['machine'], db_info['username'], db_info['password'])
-        db_dir = db_info['machine'] + "/db/data"
-        graph = Graph(db_dir)
 
-        try:
-            ndb.json2neo4j(str(json_file), graph)
-        except:
-            logging.error('Error involving loading data from json file to Neo4j database')
-            raise
+    if args.load:
+        tmpfile = tempfile.NamedTemporaryFile()
+        with tempfile.NamedTemporaryFile(suffix=".json") as fp:
+            json.dump(OrderedDict([('vertices', map(dict, nodes.values())), ('edges', edges)]), fp, indent=4)
+
+            logging.info('Connecting to Neo4j and authenticating user credentials')
+            authenticate(db_info['machine'], db_info['username'], db_info['password'])
+            db_dir = db_info['machine'] + "/db/data"
+            graph = Graph(db_dir)
+
+            ndb.json2neo4j(fp.name, graph)
+    else:
+        with sys.stdout as fp:
+            json.dump(OrderedDict([('vertices', map(dict, nodes.values())), ('edges', edges)]), fp, indent=4)
