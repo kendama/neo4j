1cf8ec18e62db81b8390d277339d09f60e328b93
kdaily <kdaily@users.noreply.github.com>
2017-03-02 12:56:25 -0800

    abstract it

--- json2neo4j.sh
+++ json2neo4j.sh
@@ -1,8 +1,13 @@
-projId=syn4921369
+projId=${1}
 
-python json2csv.py ${projId}.json --edge_csv ${projId}_edges.csv --node_csv ${projId}_nodes.csv
+json2csv.py ${projId}.json --edge_csv ${projId}_edges.csv --node_csv ${projId}_nodes.csv
 
-head -n 1 ${projId}_nodes.csv | sed "s/_id/_id:ID/" > ${projId}_nodes_UsedURL.csv
+sed -i -e "s/_id/_id:ID/" ${projId}_nodes.csv
+
+head -n 1 ${projId}_nodes.csv > ${projId}_nodes_UsedURL.csv
+head -n 1 ${projId}_nodes.csv > ${projId}_nodes_Activity.csv
+head -n 1 ${projId}_nodes.csv > ${projId}_nodes_Entity.csv
+
 grep org.sagebionetworks.repo.model.provenance.UsedURL ${projId}_nodes.csv >> ${projId}_nodes_UsedURL.csv
 grep org.sagebionetworks.repo.model.provenance.Activity ${projId}_nodes.csv >> ${projId}_nodes_Activity.csv
 grep org.sagebionetworks.repo.model.FileEntity ${projId}_nodes.csv >> ${projId}_nodes_Entity.csv
@@ -14,5 +19,4 @@ for x in generatedBy used executed ; do
     grep ${x} ${projId}_edges.csv >> ${projId}_edges_${x}.csv ;
 done
 
-~/neo4j-community-3.1.0/bin/neo4j-import --into ~/neo4j-community-3.1.0/data/databases/graph.db --nodes:Entity ${projId}_nodes_Entity.csv --nodes:Entity ${projId}_nodes_UsedURL.csv --nodes:Activity ${projId}_nodes_Activity.csv --relationships:GENERATED_BY ${projId}_edges_generatedBy.csv --relationships:USED ${projId}_edges_used.csv --relationships:EXECUTED ${projId}_edges_executed.csv
-826  history
+neo4j-import --into ~/neo4j-community-3.1.0/data/databases/graph.db --nodes:Entity ${projId}_nodes_Entity.csv --nodes:Entity ${projId}_nodes_UsedURL.csv --nodes:Activity ${projId}_nodes_Activity.csv --relationships:GENERATED_BY ${projId}_edges_generatedBy.csv --relationships:USED ${projId}_edges_used.csv --relationships:EXECUTED ${projId}_edges_executed.csv
