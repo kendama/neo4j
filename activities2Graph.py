daa62543310b78c02475fc2428019507ea5d5f79
kdaily <kenneth.daily@sagebase.org>
2017-05-31 10:09:34 -0700

    remove unused dependencies and auto-load to neo4j (too slow)

--- activities2Graph.py
+++ activities2Graph.py
@@ -9,9 +9,7 @@ import sys
 from collections import OrderedDict
 
 import synapseclient
-from py2neo import Graph, authenticate
 
-import GraphToNeo4j
 import synapsegraph
 
 logging.basicConfig(level=logging.INFO)
@@ -20,16 +18,12 @@ logger = logging.getLogger(__name__)
 if __name__ == '__main__':
     import os
 
-
     parser = argparse.ArgumentParser(description='Convert Synapse projects to JSON for import into neo4j graph database.')
     parser.add_argument('id', metavar='synId', nargs='*',
                         help='Input the synapse ID or list of synapse IDs. If no IDs given, all available Synapse projects will be used.')
     parser.add_argument('-n', type=int,
                         help='Specify the pool size for the multiprocessing module',
                         default=2)
-    parser.add_argument('-l', '--load', action='store_true',
-                        default=False,
-                        help='Load data from json file to Neo4j database')
     parser.add_argument('--debug', action='store_true', default=False,
                         help='Turn on debugging output.')
     args = parser.parse_args()
@@ -46,11 +40,6 @@ if __name__ == '__main__':
     else:
         logger.setLevel(logging.INFO)
         synapsegraph.logger.setLevel(logging.INFO)
-
-    if args.load:
-        with open(os.path.join(os.path.expanduser("~"), "credentials.json")) as creds:
-            db_info = json.load(creds)
-            logger.info("Loaded neo4j credentials.")
 
     if not args.id:
         logger.warn("No Project ids given, getting all projects from Synapse.")
@@ -78,17 +67,5 @@ if __name__ == '__main__':
     edges = synapsegraph.buildEdgesfromActivities(nodes, activities)
     logger.info('I have  %i nodes and %i edges' %(len(nodes), len(edges)))
 
-    if args.load:
-        tmpfile = tempfile.NamedTemporaryFile()
-        with tempfile.NamedTemporaryFile(suffix=".json") as fp:
-            json.dump(OrderedDict([('vertices', map(dict, nodes.values())), ('edges', edges)]), fp, indent=4)
-
-            logger.info('Connecting to Neo4j and authenticating user credentials')
-            authenticate(db_info['machine'], db_info['username'], db_info['password'])
-            db_dir = db_info['machine'] + "/db/data"
-            graph = Graph(db_dir)
-
-            GraphToNeo4j.json2neo4j(fp.name, graph)
-    else:
-        with sys.stdout as fp:
-            json.dump(OrderedDict([('vertices', map(dict, nodes.values())), ('edges', edges)]), fp, indent=4)
+    with sys.stdout as fp:
+        json.dump(OrderedDict([('vertices', map(dict, nodes.values())), ('edges', edges)]), fp, indent=4)
